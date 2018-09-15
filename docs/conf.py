17176248c815e30a10aae3ae6e2cb3fd47ee3a50
Varkal <moreau.romain83@gmail.com>
2018-09-16 00:11:24 +0200

    docs(style): change documentation theme

--- docs/conf.py
+++ docs/conf.py
@@ -75,7 +75,7 @@ language = None
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 
 # -- Options for HTML output -------------------------------------------------
@@ -83,14 +83,14 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
 #
 html_theme_options = {
-    "page_width": "1366px"
+    # "page_width": "1366px"
 }
 
 # Add any paths that contain custom static files (such as style sheets) here,
